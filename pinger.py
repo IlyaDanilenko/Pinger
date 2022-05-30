201f5778bcba308674c1611d6be9e5314ea89250
Ilya Danilenko <ilya@eadsoft.com>
2022-05-30 13:43:17 +0300

    Update pinger.py

--- pinger.py
+++ pinger.py
@@ -4,9 +4,8 @@ from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QLa
 from pyqtgraph import PlotWidget, AxisItem, mkPen
 from threading import Thread
 import json, sys, ping3
+from time import sleep, time
 ping3.EXCEPTIONS = True
-
-SYNC = True # синхронизируются ли пинги
 
 TIME = 1000 # время пинга (в мс)
 TIMEOUT = TIME - 200 # время после которого устройство считается недоступным
@@ -20,20 +19,39 @@ GRAPHIC_WIDTH = 5 # толщина линии
 SHOW_X_AXIS = False # показывать ли значения по оси X
 SCROLL = False # Полоска скрола
 
-def get_time_by_ip(ip):
-    try:
-        return int(ping3.ping(ip, timeout=TIMEOUT * 0.001) * 1000)
-    except:
-        return None
-
 class Device:
     def __init__(self, name, ip):
         self.name = name
         self.ip = ip
 
+class PingThread(Thread):
+    def __init__(self, ip):
+        self.result = TIMEOUT
+        self.__run = True
+        self.__ip = ip
+        super().__init__()
+
+    def run(self):
+        while self.__run:
+            self.result, delta = self.get_time_by_ip()
+            sleep(1 - delta)
+
+    def get_time_by_ip(self):
+        start = time()
+        try:
+            return int(ping3.ping(self.__ip, timeout=TIMEOUT * 0.001) * 1000), time() - start
+        except:
+            return None, time() - start
+
+    def stop(self):
+        self.__run = False
+
 class PingWidget(QWidget):
     def __init__(self, device, screen, count):
         self.__device = device
+
+        self.__count = 0
+        self.__sum = 0
 
         size = screen.height() // count // 3
 
@@ -48,7 +66,7 @@ class PingWidget(QWidget):
         self.ip_label = QLabel(self.__device.ip.ljust(16))
         self.ip_label.setFont(names_font)
 
-        self.ping_label = QLabel("NA".ljust(4), self)
+        self.ping_label = QLabel("NA".rjust(3) + "\\" + "NA".ljust(3), self)
         self.ping_label.setFont(QFont('Monospace', size))
         self.graph = PlotWidget(self)
         self.graph.setMouseEnabled(False, False)
@@ -63,6 +81,9 @@ class PingWidget(QWidget):
             new_axis = {"bottom":AxisItem(orientation='bottom', showValues=False, pen=mkPen(color=GRAPHIC_COLOR, width=3)), "left":self.graph.getPlotItem().getAxis('left')}
             self.graph.getPlotItem().setAxisItems(new_axis)
         self.data_line =  self.graph.plot(self.x, self.y, pen=self.pen)
+
+        self.__ping_thread = PingThread(self.__device.ip)
+        self.__ping_thread.start()
 
         self.timer = QTimer()
         self.timer.setInterval(TIME)
@@ -90,23 +111,24 @@ class PingWidget(QWidget):
         self.show()
 
     def __timer_handle(self):
-        if SYNC:
-            self.update_plot_data()
-        else:
-            Thread(target=self.update_plot_data).start()
+        Thread(target=self.update_plot_data).start()
 
     def update_plot_data(self):
-        time = get_time_by_ip(self.__device.ip)
+        time = self.__ping_thread.result
         if time is None:
-            self.ping_label.setText("NA".ljust(4))
-            self.__change_color(QColor(255, 0, 0))
             time = TIMEOUT
+            self.__sum += time
+            self.__count += 1
+            self.ping_label.setText("NA".rjust(3) + "\\" + str(self.__sum // self.__count).ljust(3))
+            self.__change_color(QColor(255, 0, 0))
         else:
+            self.__sum += time
+            self.__count += 1
             if time <= GREEN_RANGE:
                 self.__change_color(QColor(0, 255, 0))
             elif time <= YELLOW_RANGE:
                 self.__change_color(QColor(255, 255, 0))
-            self.ping_label.setText(f"{time}".ljust(4))
+            self.ping_label.setText(str(time).rjust(3) + "\\" + str(self.__sum // self.__count).ljust(3))
 
         self.x = self.x[1:]
         self.x.append(self.x[-1] + 1)
@@ -122,10 +144,18 @@ class PingWidget(QWidget):
         p.setColor(self.backgroundRole(), color)
         self.setPalette(p)
 
+    def zero_mid(self):
+        self.__sum = 0
+        self.__count = 0
+
+    def close_connection(self):
+        self.__ping_thread.stop()
+
 class MainWindow(QMainWindow):
     def __init__(self, screen, config_file):
         self.__config_file = config_file
         self.__devices = []
+        self.__devices_widget = []
         self.__screen = screen
         self.__load()
 
@@ -138,8 +168,8 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout()
         for device in self.__devices:
-            deviceWidget = PingWidget(device, self.__screen , len(self.__devices))
-            layout.addWidget(deviceWidget)
+            self.__devices_widget.append(PingWidget(device, self.__screen , len(self.__devices)))
+            layout.addWidget(self.__devices_widget[-1])
 
         widget.setLayout(layout)
 
@@ -157,11 +187,16 @@ class MainWindow(QMainWindow):
     def keyReleaseEvent(self, event):
         if event.key() == Qt.Key_Escape:
             self.close()
+            for index in range(len(self.__devices_widget)):
+                self.__devices_widget[index].close_connection()
         elif event.key() == Qt.Key_F:
             if self.isFullScreen():
                 self.showMaximized()
             else:
                 self.showFullScreen()
+        elif event.key() == Qt.Key_Z:
+            for index in range(len(self.__devices_widget)):
+                self.__devices_widget[index].zero_mid()
 
     def __load(self):
         with open(self.__config_file) as f:
